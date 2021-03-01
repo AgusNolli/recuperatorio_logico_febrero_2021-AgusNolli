9c085e3484f9d5b6a9cb04546b9d005a843a150a
Agustin Nolli <nolliagustin@gmail.com>
2021-03-01 19:03:18 -0300

    Commit1

---
+++ solucion.pl
@@ -0,0 +1,50 @@
+
+% rata(nombre,domicilio).
+rata(remy, gusteaus).
+rata(emile, bar).
+rata(django, pizzeria).
+
+% cocina(nombre, platillo, exp).
+cocina(linguini, ratatouille, 3).
+cocina(linguini, sopa, 5). 
+cocina(colette, salmonAsado, 9).
+cocina(horst, ensaladaRusa, 8).
+cocina(ramsey, sushi, 8).
+
+% trabajaEn(restaurante, empleado).
+trabajaEn(gusteaus, linguini).
+trabajaEn(gusteaus, colette).
+trabajaEn(gusteaus, skinner).
+trabajaEn(gusteaus, horst).
+trabajaEn(cafeDes2Moulins, amelie).
+trabajaEn(bar, ramsey).
+
+% 1)
+
+inspeccionSatisfactoria(Restaurante) :-
+    not(rata(Rata, Restaurante)).
+
+% 2)
+
+chef(Empleado, Restaurante) :- 
+    trabajaEn(Restaurante, Empleado),
+    cocina(Empleado, _, _).
+
+% 3)
+
+chefcito(Rata) :-
+    rata(Rata,Restaurante),
+    trabajaEn(Restaurante, linguini).
+
+% 4)
+
+cocinaBien(Persona, Platillo) :-
+    cocina(Persona, Platillo, Exp),
+    Exp > 7.
+    
+cocinaBien(remi, _).
+
+% 5)
+
+seEspecializaEn(Restaurante, Platillo) :-
+    forall(cocinaBien(Empleado,Platillo), trabajaEn(Restaurante,Empleado)).
